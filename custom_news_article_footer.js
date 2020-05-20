a2fda4f0a7f8474c4e5cf7dc5c74e05df5e30c8f
Andy Keller <adkeller@gmail.com>
2020-05-20 16:19:53 -0400

    Move ad below photo for specific news post

--- custom_news_article_footer.js
+++ custom_news_article_footer.js
@@ -2,3 +2,11 @@
 function hasClass(element, cls) {return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;}
 var bodyclass = document. getElementsByTagName('body')[0];
 // if body has this class, run the first script, if not run the global script
+
+if (hasClass(bodyclass, 'postid-40870')) {
+    jQuery(function($){
+        $("div.article__content > aside.ad-block").addClass('ad2');
+        $("div.article__content > p").slice(4, 6).addClass('move');
+        $(".ad2").before($(".move "));
+    });
+}
