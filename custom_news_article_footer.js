da4d9faf7c6a26d39b7a14cbb8202059106c4d7b
Andy Keller <adkeller@gmail.com>
2020-05-20 15:09:23 -0400

    News Article - add function for jquery

--- custom_news_article_footer.js
+++ custom_news_article_footer.js
@@ -2,6 +2,8 @@
 function hasClass(element, cls) {return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;}
 var bodyclass = document. getElementsByTagName('body')[0];
 // if body has this class, run the first script, if not run the global script
+jQuery(function($){
 if (hasClass(bodyclass, 'postid-40870')) {
     $("div.article__image").before($("aside.ad-block"));
 }
+});
