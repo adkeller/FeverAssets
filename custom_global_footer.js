5e742aa8ec7f11f6470df96ba1938f3a30eabec9
Andy Keller <adkeller@gmail.com>
2020-05-20 15:16:37 -0400

    Remove js to find body class from global footer

--- custom_global_footer.js
+++
@@ -1,3 +0,0 @@
-function hasClass(element, cls) {return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;}
-var bodyclass = document. getElementsByTagName('body')[0];
-// if body has this class, run the first script, if not run the global script
