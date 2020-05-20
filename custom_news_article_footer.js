94efdf3efdf1075526d4377d6430832a532db231
Andy Keller <adkeller@gmail.com>
2020-05-20 15:13:24 -0400

    Remove Ad switch

--- custom_news_article_footer.js
+++ custom_news_article_footer.js
@@ -2,8 +2,3 @@
 function hasClass(element, cls) {return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;}
 var bodyclass = document. getElementsByTagName('body')[0];
 // if body has this class, run the first script, if not run the global script
-jQuery(function($){
-if (hasClass(bodyclass, 'postid-40870')) {
-    $("div.article__image:first").before($("aside.ad-block"));
-}
-});
