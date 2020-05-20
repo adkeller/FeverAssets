6ea8144c0c6ec7dac1542359da15c141c7c2776b
Andy Keller <adkeller@gmail.com>
2020-05-20 15:11:33 -0400

    Select First Ad

--- custom_news_article_footer.js
+++ custom_news_article_footer.js
@@ -4,6 +4,6 @@ var bodyclass = document. getElementsByTagName('body')[0];
 // if body has this class, run the first script, if not run the global script
 jQuery(function($){
 if (hasClass(bodyclass, 'postid-40870')) {
-    $("div.article__image").before($("aside.ad-block"));
+    $("div.article__image:first").before($("aside.ad-block"));
 }
 });
